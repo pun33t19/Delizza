76e62f0422e580d9fbdd8fbf10f4dc3cf84ac804
pun33t19 <puneet22088@gmail.com>
2022-05-11 14:00:31 +0530

    bottom navigation implemented

--- app/src/main/java/com/example/delizza/MainActivity.kt
+++ app/src/main/java/com/example/delizza/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.delizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController=navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
